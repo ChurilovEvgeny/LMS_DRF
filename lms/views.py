b6938746972de301aa8556c0f94e282f216628b1
Churilov Evgeny <i@churilovevgeny.ru>
2024-08-19 19:51:06 +0300

    Добавлены Дженерики для Урока

--- lms/views.py
+++ lms/views.py
@@ -1,10 +1,33 @@
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 
-from lms.models import Course
-from lms.serializers import CourseSerializer
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
-    
+
+
+class LessonCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDeleteAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
