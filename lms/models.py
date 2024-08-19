2a842c56b5c340406b4fa7e760ed582df8658fda
Churilov Evgeny <i@churilovevgeny.ru>
2024-08-19 17:47:56 +0300

    Добавлены модели курса и урока, сделана миграция

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from utils.utils import generate_filename_course_preview, generate_filename_lesson_preview
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование курса")
+    preview = models.ImageField(upload_to=generate_filename_course_preview, verbose_name="Аватар", **NULLABLE)
+    description = models.TextField(verbose_name="Описание курса", **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование урока")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс", related_name="lessons")
+    description = models.TextField(verbose_name="Описание урока", **NULLABLE)
+    preview = models.ImageField(upload_to=generate_filename_lesson_preview, verbose_name="Аватар", **NULLABLE)
+    video_url = models.URLField(verbose_name="Ссылка на видео", **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,3 @@
-
 import pathlib
 import uuid
 
@@ -7,6 +6,13 @@ def generate_filename_user_avatar(instance, filename):
     return generate_filename(instance, filename, 'users')
 
 
+def generate_filename_course_preview(instance, filename):
+    return generate_filename(instance, filename, 'courses')
+
+
+def generate_filename_lesson_preview(instance, filename):
+    return generate_filename(instance, filename, 'lessons')
+
+
 def generate_filename(instance, filename, subdir):
     return pathlib.Path(subdir) / f"{uuid.uuid4().hex}.{filename.split('.')[-1]}"
-
