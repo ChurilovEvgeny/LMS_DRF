cbaaf1e35dc4e19ddc9e3205c68ede6ae9e53cf3
Churilov Evgeny <i@churilovevgeny.ru>
2024-09-03 11:37:43 +0300

    Для курсов и уроков добавлена валижация на сторонние сайты, кроме YouTube

--- lms/serializers.py
+++ lms/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 from lms.models import Course, Lesson
+from lms.validators import OnlyYouTubeLinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+        validators = [OnlyYouTubeLinkValidator(field='description'), OnlyYouTubeLinkValidator(field='video_url')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -20,3 +22,4 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("pk", "name", "preview", "description", "lessons_count", "lessons")
+        validators = [OnlyYouTubeLinkValidator(field='description')]
