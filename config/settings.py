33db83dcf22b862acb19fda82dfa215a0ef3fd38
Churilov Evgeny <i@churilovevgeny.ru>
2024-09-05 14:40:53 +0300

    Добавлен CORS

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     "rest_framework_simplejwt",
+    "corsheaders",
     "lms",
     "users",
 ]
@@ -38,6 +39,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -128,3 +130,13 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "<http://localhost:8000>",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "<http://localhost:8000>",
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
