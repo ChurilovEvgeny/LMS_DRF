3498c06656c5385cc581a73550904d794f525595
Churilov Evgeny <i@churilovevgeny.ru>
2024-09-05 18:43:17 +0300

    Fix CORS

--- config/settings.py
+++ config/settings.py
@@ -132,11 +132,11 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "<http://localhost:8000>",
+    "http://localhost:8000",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "<http://localhost:8000>",
+    "http://localhost:8000",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
